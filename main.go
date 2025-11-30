dfe9b34c04a49162c37a9476dfcc4f7fe2cda213
Darien Miller <darienmiller88@yahoo.com>
2025-11-30 13:18:39 -0500

    chore: added comments to main.go

--- main.go
+++ main.go
@@ -13,12 +13,16 @@ import (
 )
 
 func main(){
+	//Load env file immediately at the start of the program
 	godotenv.Load()
 
+	//Create new chi router instance to push handlers to.
 	router := chi.NewRouter()
 
+	//Middleware stack, keeping it basic for now.
 	router.Use(middleware.Logger, middleware.Recoverer)
 	
+	//Initiate the database connection to mongodb, and defer its disconnection.
 	database.Init()
 	defer database.DisconnectClient()
 
@@ -26,7 +30,7 @@ func main(){
 		res.Write([]byte("Hello world!"))
 	})
 
-	
+	//Finally, listen and serve on the port in the env, which is 8080 on local machine.
 	fmt.Println("Listening on Port:", os.Getenv("PORT"))
 	http.ListenAndServe(fmt.Sprintf(":%s", os.Getenv("PORT")), router)
 }
