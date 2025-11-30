249bedc30ac652c43b255a2243bbaf58c5e52ee5
Darien Miller <darienmiller88@yahoo.com>
2025-11-30 11:48:40 -0500

    feat: database is connected!!

--- main.go
+++ main.go
@@ -8,6 +8,8 @@ import (
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 	"github.com/joho/godotenv"
+
+	"JeopardyScoreBoardV2/database"
 )
 
 func main(){
@@ -16,10 +18,14 @@ func main(){
 	router := chi.NewRouter()
 
 	router.Use(middleware.Logger, middleware.Recoverer)
+	
+	database.Init()
+	defer database.DisconnectClient()
 
 	router.Get("/", func(res http.ResponseWriter, req *http.Request) {
 		res.Write([]byte("Hello world!"))
 	})
+
 	
 	fmt.Println("Listening on Port:", os.Getenv("PORT"))
 	http.ListenAndServe(fmt.Sprintf(":%s", os.Getenv("PORT")), router)
