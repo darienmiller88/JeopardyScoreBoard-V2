9c97c8ce85dc5d2f4a7b2fed43a4925dfe9da518
Darien Miller <darienmiller88@yahoo.com>
2025-11-30 10:23:29 -0500

    feat: server is now running

--- go.mod
+++ go.mod
@@ -1,3 +1,14 @@
 module JeopardyScoreBoardV2
 
 go 1.25.0
+
+require (
+	github.com/go-chi/chi/v5 v5.2.3 // indirect
+	github.com/go-chi/httprate v0.15.0 // indirect
+	github.com/go-ozzo/ozzo-validation/v4 v4.3.0 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/klauspost/cpuid/v2 v2.2.10 // indirect
+	github.com/zeebo/xxh3 v1.0.2 // indirect
+	go.mongodb.org/mongo-driver v1.17.6 // indirect
+	golang.org/x/sys v0.30.0 // indirect
+)
--- main.go
+++ main.go
@@ -2,8 +2,22 @@ package main
 
 import (
 	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/joho/godotenv"
 )
 
 func main(){
-	fmt.Println("Hello world")
+	godotenv.Load()
+
+	router := chi.NewRouter()
+
+	router.Get("/", func(res http.ResponseWriter, req *http.Request) {
+		res.Write([]byte("Hello world!"))
+	})
+	
+	fmt.Println("Listening on Port:", os.Getenv("PORT"))
+	http.ListenAndServe(fmt.Sprintf(":%s", os.Getenv("PORT")), router)
 }
