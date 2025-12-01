9e4cc51446649e18bb966e00387ac4c9810cbe72
Darien Miller <darienmiller88@yahoo.com>
2025-11-30 21:48:45 -0500

    fix: completed view controller mount in index file, and added comments

--- controllers/Index.go
+++ controllers/Index.go
@@ -6,8 +6,16 @@ import (
 
 type Index struct{
 	Router *chi.Mux
+	viewsController ViewsController
 }
 
 func (i *Index) Init(){
 	i.Router = chi.NewRouter()
+
+	//Initialize the views controller
+	i.viewsController.Init()
+
+	//Afterwards, mount the views router onto this router, which wiil be mounted onto the main chi router
+	//in main.go
+	i.Router.Mount("/", i.viewsController.Router)
 }
