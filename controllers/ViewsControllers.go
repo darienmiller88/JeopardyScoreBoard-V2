2537c5cc25571f287544099ab622f988db390523
Darien Miller <darienmiller88@yahoo.com>
2025-12-07 23:38:34 -0500

    fix: removed old pages templates that was causing nil reference, template map working!

--- controllers/ViewsControllers.go
+++ controllers/ViewsControllers.go
@@ -7,20 +7,17 @@ import (
 	"os"
 	"strings"
 
-	"github.com/foolin/goview"
 	"github.com/go-chi/chi/v5"
 )
 
 type ViewsController struct{
-	pagesTemplate *template.Template
-	templates      map[string]*template.Template
-	Router        *chi.Mux
+	templates map[string]*template.Template
+	Router    *chi.Mux
 }
 
 func (v *ViewsController) Init(){
 	v.Router        = chi.NewRouter()
 	v.templates     = make(map[string]*template.Template)
-	v.pagesTemplate = template.Must(template.ParseGlob("templates/*.html"))
 
 	//Initialize template map
 	v.InitTemplateMap()
@@ -38,47 +35,39 @@ func (v *ViewsController) InitTemplateMap(){
 	entries, err := os.ReadDir("./templates/pages")
 
 	if err != nil {
-		fmt.Println("err:", err)
+		panic(err)
 	}else{
 		for _, entry := range entries{
-				name, _ := strings.CutSuffix(entry.Name(), ".html")
-				v.templates[name] = template.Must(template.ParseFiles("templates/Base.html", fmt.Sprintf("templates/pages/%s.html", name)))
+			name, _ := strings.CutSuffix(entry.Name(), ".html")
+			v.templates[name] = template.Must(template.ParseFiles("templates/Base.html", fmt.Sprintf("templates/pages/%s.html", name)))
 		}
 	}
 }
 
 func (v *ViewsController) CreateGame(res http.ResponseWriter, req *http.Request){
-	if err := goview.Render(res, http.StatusOK, "CreateGame", nil); err != nil{
+	if err := v.templates["CreateGame"].Execute(res, nil); err != nil{
 		http.Error(res, err.Error(), http.StatusInternalServerError)
 	}
 }
 
 func (v *ViewsController) TeamMode(res http.ResponseWriter, req *http.Request){
-	if err := v.pagesTemplate.ExecuteTemplate(res, "Base", nil); err != nil{
+	if err := v.templates["TeamMode"].Execute(res, nil); err != nil{
 		http.Error(res, err.Error(), http.StatusInternalServerError)
 	}
 }
 
 func (v *ViewsController) AddPlayer(res http.ResponseWriter, req *http.Request){
-	if err := v.pagesTemplate.ExecuteTemplate(res, "AddPlayer.html", nil); err != nil{
-		http.Error(res, err.Error(), http.StatusInternalServerError)
-	}
+	
 }
 
 func (v *ViewsController) ViewGames(res http.ResponseWriter, req *http.Request){
-	if err := v.pagesTemplate.ExecuteTemplate(res, "Base", nil); err != nil{
-		http.Error(res, err.Error(), http.StatusInternalServerError)
-	}
+	
 }
 
 func (v *ViewsController) LogIn(res http.ResponseWriter, req *http.Request){
-	if err := v.pagesTemplate.ExecuteTemplate(res, "Base", nil); err != nil{
-		http.Error(res, err.Error(), http.StatusInternalServerError)
-	}
+	
 }
 
 func (v *ViewsController) NotFound(res http.ResponseWriter, req *http.Request){
-	if err := v.pagesTemplate.ExecuteTemplate(res, "Base", nil); err != nil{
-		http.Error(res, err.Error(), http.StatusInternalServerError)
-	}
+	
 }
