93c4aa79cd0d7d75b7d160001fa2555f055675cd
Darien Miller <darienmiller88@yahoo.com>
2025-12-08 23:55:36 -0500

    progress: added AddLocation and GetLocation to interface for locationRepository

--- repositories/LocationRepository.go
+++ repositories/LocationRepository.go
@@ -1,11 +1,13 @@
 package repositories
 
 import(
+	"JeopardyScoreBoardV2/models"
 	"go.mongodb.org/mongo-driver/v2/mongo"
 )
 
 type LocationRepository interface{
-
+	AddLocation() models.Result[models.Location]
+	GetLocation() models.Result[models.Location]
 }
 
 type MongoLocationRepository struct{
