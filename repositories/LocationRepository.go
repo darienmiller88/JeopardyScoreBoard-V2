791249cfc2ad1c377a92d56f9b785e96d31700ae
Darien Miller <darienmiller88@yahoo.com>
2025-12-09 16:01:44 -0500

    progress: completed AddLocation() function in Location repo

--- repositories/LocationRepository.go
+++ repositories/LocationRepository.go
@@ -3,7 +3,9 @@ package repositories
 import (
 	"JeopardyScoreBoardV2/models"
 	"context"
+	"net/http"
 
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/v2/mongo"
 )
 
@@ -15,9 +17,9 @@ const(
 //LocationRepository interface to allow mocking when testing the service. The test can provide the service 
 //a dummy implementation 
 type LocationRepository interface{
-	AddLocation(ctx context.Context)  models.Result[models.Location]
-	GetLocations(ctx context.Context) models.Result[models.Location]
-	GetLocation(ctx context.Context, locationName string) models.Result[models.Location]
+	AddLocation(ctx context.Context, location models.Location) models.Result[models.Location]
+	GetLocation(ctx context.Context, locationName string)      models.Result[models.Location]
+	GetLocations(ctx context.Context)                          models.Result[models.Location]
 }
 
 type MongoLocationRepository struct{
@@ -29,8 +31,25 @@ func GetMongoLocationCollection(newCollection *mongo.Collection) *MongoLocationR
 	return &MongoLocationRepository{ locationCollection: newCollection.Clone() }
 }
 
-func (m *MongoLocationRepository) AddLocation() models.Result[models.Location]{
-	return models.Result[models.Location]{}
+//Add a new adapt location, which for now, I will not expose.
+func (m *MongoLocationRepository) AddLocation(ctx context.Context, location models.Location) models.Result[models.Location]{
+	insertOneResult, err := m.locationCollection.InsertOne(ctx, location)
+	locationResult := models.Result[models.Location]{}
+
+	//If there was an error inserting the location, return it along a 500
+	if err != nil{
+		locationResult.Err = err
+		locationResult.StatusCode = http.StatusInternalServerError
+
+		return locationResult
+	}
+	
+	//Afterwards, take the inserted id of the location, add it to the payload, and return it with a 200
+	location.ID = insertOneResult.InsertedID.(primitive.ObjectID)
+	locationResult.ResultData = location
+	locationResult.StatusCode = http.StatusOK
+
+	return locationResult
 }
 
 func (m *MongoLocationRepository) GetLocations() models.Result[models.Location]{
