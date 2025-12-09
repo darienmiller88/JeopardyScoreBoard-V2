b89a445379c3cd4be131706b9ca6c5791791e50e
Darien Miller <darienmiller88@yahoo.com>
2025-12-09 09:38:55 -0500

    progress: added GetLocations() to LocationRepo interface, and added helper function to get new instance of Mongo Location repo

--- repositories/LocationRepository.go
+++ repositories/LocationRepository.go
@@ -2,18 +2,22 @@ package repositories
 
 import(
 	"JeopardyScoreBoardV2/models"
+	"JeopardyScoreBoardV2/database"
 	"go.mongodb.org/mongo-driver/v2/mongo"
 )
 
 type LocationRepository interface{
-	AddLocation() models.Result[models.Location]
-	GetLocation() models.Result[models.Location]
+	AddLocation()                    models.Result[models.Location]
+	GetLocations()                   models.Result[models.Location]
+	GetLocation(locationName string) models.Result[models.Location]
 }
 
 type MongoLocationRepository struct{
 	locationCollection *mongo.Collection
+
 }
 
-func GetMongoLocationCollection(newMongoCollection *mongo.Collection) *MongoLocationRepository{
-	return &MongoLocationRepository{ locationCollection: newMongoCollection }
+//Receive a new instance of Location repository using a mongo collection as the database. 
+func GetMongoLocationCollection() *MongoLocationRepository{
+	return &MongoLocationRepository{ locationCollection: database.GetLocationsCollection().Clone() }
 }
