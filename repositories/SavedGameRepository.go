326d4194c791f2154a9c6f612953cb8639964666
Darien Miller <darienmiller88@yahoo.com>
2025-12-10 18:56:20 -0500

    progress: added saved game repository, and intializer function to SavedGameRepository.go

--- repositories/SavedGameRepository.go
+++ repositories/SavedGameRepository.go
@@ -2,15 +2,23 @@ package repositories
 
 import (
 	"JeopardyScoreBoardV2/models"
-	"net/http"
+	"context"
 
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/v2/mongo"
 )
 
 type SavedGameRepository interface {
-	GetAllSavedGames(req *http.Request) models.Result[[]models.SavedGame]
-	GetAllSavedGamesFromLocation(req *http.Request, locationName string) models.Result[[]models.SavedGame]
-	DeleteSavedGame(req *http.Request, savedGameId primitive.ObjectID) models.Result[*mongo.DeleteResult]
-	AddSavedGame(req *http.Request, savedGame models.SavedGame) models.Result[models.SavedGame]
+	GetAllSavedGames(ctx context.Context) models.Result[[]models.SavedGame]
+	GetAllSavedGamesFromLocation(ctx context.Context, locationName string) models.Result[[]models.SavedGame]
+	DeleteSavedGame(ctx context.Context, savedGameId primitive.ObjectID) models.Result[*mongo.DeleteResult]
+	AddSavedGame(ctx context.Context, savedGame models.SavedGame) models.Result[models.SavedGame]
+}
+
+type MongoSavedGameRepository struct{
+	savedGameCollection *mongo.Collection
+}
+
+func GetNewMongoSavedGameRepository(newCollection *mongo.Collection) *MongoSavedGameRepository{
+	return &MongoSavedGameRepository{ savedGameCollection: newCollection }
 }
