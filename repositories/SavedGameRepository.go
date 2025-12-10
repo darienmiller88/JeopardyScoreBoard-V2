dbe436709e779073428320fdb7e9b557ecc73b87
Darien Miller <darienmiller88@yahoo.com>
2025-12-10 18:46:28 -0500

    progress: added function definitions to SavedGameRepository interface

--- repositories/SavedGameRepository.go
+++ repositories/SavedGameRepository.go
@@ -1,5 +1,16 @@
 package repositories
 
-type SavedGameRepository interface{
-	
+import (
+	"JeopardyScoreBoardV2/models"
+	"net/http"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/v2/mongo"
+)
+
+type SavedGameRepository interface {
+	GetAllSavedGames(req *http.Request) models.Result[[]models.SavedGame]
+	GetAllSavedGamesFromLocation(req *http.Request, locationName string) models.Result[[]models.SavedGame]
+	DeleteSavedGame(req *http.Request, savedGameId primitive.ObjectID) models.Result[*mongo.DeleteResult]
+	AddSavedGame(req *http.Request, savedGame models.SavedGame) models.Result[models.SavedGame]
 }
