9d38c218e1a41c260924f74cb0bf42320cf564e1
Darien Miller <darienmiller88@yahoo.com>
2025-12-10 19:19:40 -0500

    feat: completed GetAllSavedGames() in SavedGameRepository.go

--- repositories/SavedGameRepository.go
+++ repositories/SavedGameRepository.go
@@ -3,7 +3,9 @@ package repositories
 import (
 	"JeopardyScoreBoardV2/models"
 	"context"
+	"net/http"
 
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/v2/mongo"
 )
@@ -24,18 +26,40 @@ func GetNewMongoSavedGameRepository(newCollection *mongo.Collection) *MongoSaved
 	return &MongoSavedGameRepository{ savedGameCollection: newCollection }
 }
 
+//Get all Saved games from database.
 func (m *MongoSavedGameRepository) GetAllSavedGames(ctx context.Context) models.Result[[]models.SavedGame]{
-	return models.Result[[]models.SavedGame]{}
+	findResult, err := m.savedGameCollection.Find(ctx, bson.D{})
+	
+	//If there was an error retrieving all documents, return the error, and a 500
+	if err != nil {
+		return models.Result[[]models.SavedGame]{ Err: err, StatusCode: http.StatusInternalServerError }
+	}
+
+	savedGames := []models.SavedGame{}
+
+	//Unmarshall the mongo cursor into the array of saved games.
+	if err := findResult.All(ctx, &savedGames); err != nil{
+		return models.Result[[]models.SavedGame]{
+			Err: err,
+			StatusCode: http.StatusInternalServerError,
+		}
+	}
+
+	//Return the retrieved saved games.
+	return models.Result[[]models.SavedGame]{ ResultData: savedGames, StatusCode: http.StatusOK }
 }
 
+//Get all saved games played at a specific location.
 func (m *MongoSavedGameRepository) GetAllSavedGamesFromLocation(ctx context.Context, locationName string) models.Result[[]models.SavedGame]{
 	return models.Result[[]models.SavedGame]{}
 }
 
+//Delete a saved game
 func (m *MongoSavedGameRepository) DeleteSavedGame(ctx context.Context, savedGameId primitive.ObjectID) models.Result[*mongo.DeleteResult]{
 	return models.Result[*mongo.DeleteResult]{}
 }
 
+//Add a new saved game
 func (m *MongoSavedGameRepository) AddSavedGame(ctx context.Context, savedGame models.SavedGame) models.Result[models.SavedGame]{
 	return models.Result[models.SavedGame]{}
 }
