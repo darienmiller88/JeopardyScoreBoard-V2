e717aedef619e7bec034c99dd0e0e4f66ef2d9a6
unknown <darienmiller88@yahoo.com>
2025-12-24 18:56:56 -0500

    feat: migration flow finally complete!

---
+++ migrations/000004_add_locations.up.sql
@@ -0,0 +1,8 @@
+INSERT INTO Locations (location_name, borough) VALUES ('5030 Broadway', 'Manhattan');
+INSERT INTO Locations (location_name, borough) VALUES ('W 154th st', 'Manhattan');
+INSERT INTO Locations (location_name, borough) VALUES ('Flushing', 'Brooklyn');
+INSERT INTO Locations (location_name, borough) VALUES ('Lawrence', 'Brooklyn');
+INSERT INTO Locations (location_name, borough) VALUES ('Elmwood', 'Brooklyn');
+INSERT INTO Locations (location_name, borough) VALUES ('Grand Concourse', 'Bronx');
+INSERT INTO Locations (location_name, borough) VALUES ('Pelham Bay', 'Bronx');
+INSERT INTO Locations (location_name, borough) VALUES ('Port Richmond', 'Staten Island');
--- migrations/000005_add_locations.up.sql
+++
@@ -1,1 +0,0 @@
-INSERT INTO Locations VALUES(location_name) VALUES ('Manhattan')
