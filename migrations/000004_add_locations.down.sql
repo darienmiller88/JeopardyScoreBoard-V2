febe5850ab48603709d12966c20608faeba93f2e
unknown <darienmiller88@yahoo.com>
2025-12-24 19:08:16 -0500

    progress: added sql for v4 down

---
+++ migrations/000004_add_locations.down.sql
@@ -0,0 +1,8 @@
+DELETE FROM Locations WHERE location_name='Port Richmond';
+DELETE FROM Locations WHERE location_name='Pelham Bay';
+DELETE FROM Locations WHERE location_name='Grand Concourse';
+DELETE FROM Locations WHERE location_name='Elmwood';
+DELETE FROM Locations WHERE location_name='Lawrence';
+DELETE FROM Locations WHERE location_name='Flushing';
+DELETE FROM Locations WHERE location_name='W 154th st';
+DELETE FROM Locations WHERE location_name='5030 Broadway';
