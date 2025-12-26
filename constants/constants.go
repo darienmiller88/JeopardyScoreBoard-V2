ebfb47455348bd3c37f40f91bb616d872ca16bb7
unknown <darienmiller88@yahoo.com>
2025-12-25 23:50:50 -0500

    feat: added database schema

--- constants/constants.go
+++ constants/constants.go
@@ -2,10 +2,14 @@ package constants
 
 const(
 	//CREATE
-	insertNewPlayer string = "INSERT INTO players (player_name, is_active, left_at, location_id, team_id)" +
-	"VALUES(:player_name, :is_active, :left_at, :location_id, :team_id) RETURNING id"
+	insertNewPlayer string = `
+		INSERT INTO players (player_name, is_active, left_at, location_id, team_id)
+		VALUES(:player_name, :is_active, :left_at, :location_id, :team_id) RETURNING id
+	`
 
-	insertNewSavedGame
+	insertNewSavedGame string = `
+		INSERT INTO SavedGames (winner, total_score, average_score, is_team_game)
+	`
 
 	//READ
 
