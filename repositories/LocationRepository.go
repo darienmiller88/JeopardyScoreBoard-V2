341eda7c6d56a17d874a2eef63d0621d1b2186a4
unknown <darienmiller88@yahoo.com>
2025-12-28 09:50:08 -0500

    feat: change PlayerCardService to PlayerService

--- repositories/LocationRepository.go
+++ repositories/LocationRepository.go
@@ -48,38 +48,6 @@ func (s *sqlLocationRepository) GetLocation(ctx context.Context, locationName st
 	return getResult(nil, http.StatusOK, location)
 }
 
-// //Return the array of players from a particular location
-// func (m *MongoLocationRepository) GetPlayersFromLocation(ctx context.Context, locationName string) models.Result[[]models.PlayerCard]{
-// 	locationResult := m.GetLocation(ctx, locationName)
-
-// 	//Return the error from the above call if an error occurs
-// 	if locationResult.Err != nil {
-// 		return getResult(locationResult.Err, http.StatusInternalServerError, []models.PlayerCard{})
-// 	}
-
-// 	//IF not, return all of the players for for that location.
-// 	return getResult(nil, http.StatusOK, locationResult.ResultData.Players)
-// }
-
-// //Return every single player at every single location.
-// func (m *MongoLocationRepository) GetAllPlayersFromAllLocations(ctx context.Context) models.Result[[]models.PlayerCard]{
-// 	locationsResult := m.GetAllLocations(ctx)	
-
-// 	if locationsResult.Err != nil {
-// 		return getResult(locationsResult.Err, locationsResult.StatusCode, []models.PlayerCard{})
-// 	}
-
-// 	players := []models.PlayerCard{}
-
-// 	//Range over each location, and extract all of the players for each location.
-// 	for _, location := range locationsResult.ResultData {
-// 		players = append(players, location.Players...)
-// 	}
-
-// 	//Return the list of all players in the database.
-// 	return getResult(nil, http.StatusOK, players)
-// }
-
 //Helper function to allow repos to send result payloads with less text.
 func getResult[T any](err error, statusCode int, payload T) models.Result[T] {
 	return models.Result[T]{
