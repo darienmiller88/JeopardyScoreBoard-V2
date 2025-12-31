2a0a0d29c876b133dffe90a2da52a503ded9d497
unknown <darienmiller88@yahoo.com>
2025-12-30 22:12:12 -0500

    feat: moved Location and Players to sql binding

--- models/Location.go
+++ models/Location.go
@@ -4,34 +4,32 @@ import (
 	"time"
 
 	"github.com/go-ozzo/ozzo-validation/v4"
-	"go.mongodb.org/mongo-driver/v2/bson"
 )
 
-// This struct will represent the ADAPT community network location where a jeopardy game was played.
 type Location struct {
-	ID        bson.ObjectID `bson:"_id,omitempty"` 
-	CreatedAt time.Time     `bson:"created_at"` 
-	UpdatedAt time.Time     `bson:"updated_at"`
+	ID        int       `db:"id"` 
+	CreatedAt time.Time `db:"created_at"` 
+	UpdatedAt time.Time `db:"updated_at"`
 
 	//The name of the ADAPT location (Pelham Bay, Lawrence, Elmwood, etc)
-	LocationName string     `bson:"location_name"`
-
-	//Here are all of the users that played in the game.
-	Players     []Player `bson:"users"`
-}
-
-func (l *Location) InitCreatedAtAndUpdatedAt(){
-	l.CreatedAt = time.Now()
-	l.UpdatedAt = time.Now()
-
-	//If this field is not initialized, it is interpreted as "null" by mongoDB, and not an empty array.
-	l.Players = []Player{}
+	LocationName string `db:"location_name"`
 }
 
 func (l *Location) Validate() error{
 	return validation.ValidateStruct(
 		l,
-		validation.Field(&l.Players, validation.Required, validation.Length(1, 0)),
 		validation.Field(&l.LocationName, validation.Required),
 	)
 }
+// This struct will represent the ADAPT community network location where a jeopardy game was played.
+// type Location struct {
+// 	ID        bson.ObjectID `bson:"_id,omitempty"` 
+// 	CreatedAt time.Time     `bson:"created_at"` 
+// 	UpdatedAt time.Time     `bson:"updated_at"`
+
+// 	//The name of the ADAPT location (Pelham Bay, Lawrence, Elmwood, etc)
+// 	LocationName string     `bson:"location_name"`
+
+// 	//Here are all of the users that played in the game.
+// 	Players     []Player `bson:"users"`
+// }
--- models/Team.go
+++ models/Team.go
@@ -21,8 +21,7 @@ type Team struct{
 func (t *Team) Validate() error{
 	return validation.ValidateStruct(
 		t,
-		validation.Field(&t.TeamName, validation.By(t.checkTeamNameInLocations)),
-		validation.Field(&t.Players, validation.Required, validation.Length(2, 0), validation.By(t.checkDuplicateTeamPlayers), validation.By(t.checkForValidTeamPlayers)),
+		validation.Field(&t.Players, validation.Required, validation.Length(2, 0), validation.By(t.checkDuplicateTeamPlayers)),
 	)
 }
 
@@ -59,84 +58,6 @@ func (t *Team) checkDuplicateTeamPlayers (field interface{}) error{
 
 	//If not, return nil signifying validation success.
 	return nil
-}
-
-//Check to see if any of the players the client sent are invalid people at the specific ADAPT location. 
-func (t *Team) checkForValidTeamPlayers(field interface{}) error {
-	players, ok := field.([]string)
-
-	if !ok{
-		return fmt.Errorf("could not parse %T into object", field)
-	}
-
-	//Since the team name is validated first to ensure it matches a valid ADAPT location, we can just send it the 
-	//below method to get the Location object by the team name.
-	location, err := getLocationByName(t.TeamName)
-
-	if err != nil{
-		return err
-	}
-
-	//Save the amount of people in the players array sent by the client, as well as the number of people at the 
-	//actual ADAPT location.
-	numPlayers          := len(players)
-	numPeopleAtLocation := len(location.Players) 
-	
-	//If the client sent more players than actual people at this ADAPT location, send an error to prevent time wasting.
-	if numPlayers > numPeopleAtLocation {
-		return fmt.Errorf("length of players field cannot exceed total amount of people at ADAPT location: %d > %d", numPlayers, numPeopleAtLocation)
-	}
-
-	uniquePlayersMap := make(map[string]struct{})
-	
-	
-	//Afterwards, check each player the client sent to see if it exists in the map. If not, return the following error.
-	for _, newlyAddedplayer := range players{
-		if _, exists := uniquePlayersMap[newlyAddedplayer]; !exists {
-			return fmt.Errorf("'%s' is not a valid person at the ADAPT location '%s'", newlyAddedplayer, t.TeamName)
-		}
-	}
-	
-	//If every name the client sent matched the names of the players in the ADAPT location, return nil to allow
-	//validation success.
-	return nil
-}
-
-// Check to see if the team name passed in from the client actually exists as a Valid ADAPT Community Location.
-// In this case, the "team name" is just the name of the ADAPT location, not a customizable team name.
-func (t *Team) checkTeamNameInLocations(field interface{}) error{
-	locationName, ok := field.(string)
-
-	if !ok{
-		return fmt.Errorf("could not parse %T into object", field)
-	}
-
-	locations, err := getLocations()
-
-	if err != nil{
-		return err
-	}
-
-	//Retrieve all of the locations from the database.
-	for _, location := range locations {
-
-		//If the location name sent by the client matches a Valid ADAPT location, return nil as the field is valid.
-		if locationName == location.LocationName {
-			return nil
-		}
-	}
-
-	//If the location did NOT match any of the ADAPT location names, prepare this data to send back to them.
-	locationNames := make([]string, len(locations)) 
-
-	//Sadly, there is no .Map() function to extract the location name from the Location object into a array of strings.
-	for i, location := range locations {
-		locationNames[i] = location.LocationName
-	}	
-
-	//Finally, return a error message signaling to the user that the team name MUST be one of the valid ADAPT locations.
-	return fmt.Errorf("Team name '%s' is not a valid team name. Please choose from " +
-		"the following: [%s] ", locationName, strings.Join(locationNames, ", "))
 }
 
 //Function to retrieve all ADAPT locations from database. Sadly, I cannot use the service for this as that would result
