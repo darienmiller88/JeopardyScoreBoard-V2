0f6733f861a48dc2c6b858a92126679cc75f9524
unknown <darienmiller88@yahoo.com>
2026-01-02 16:20:46 -0500

    progress: removed validation from Location object

--- models/Location.go
+++ models/Location.go
@@ -2,8 +2,6 @@ package models
 
 import (
 	"time"
-
-	"github.com/go-ozzo/ozzo-validation/v4"
 )
 
 type Location struct {
@@ -14,22 +12,3 @@ type Location struct {
 	//The name of the ADAPT location (Pelham Bay, Lawrence, Elmwood, etc)
 	LocationName string `db:"location_name"`
 }
-
-func (l *Location) Validate() error{
-	return validation.ValidateStruct(
-		l,
-		validation.Field(&l.LocationName, validation.Required),
-	)
-}
-// This struct will represent the ADAPT community network location where a jeopardy game was played.
-// type Location struct {
-// 	ID        bson.ObjectID `bson:"_id,omitempty"` 
-// 	CreatedAt time.Time     `bson:"created_at"` 
-// 	UpdatedAt time.Time     `bson:"updated_at"`
-
-// 	//The name of the ADAPT location (Pelham Bay, Lawrence, Elmwood, etc)
-// 	LocationName string     `bson:"location_name"`
-
-// 	//Here are all of the users that played in the game.
-// 	Players     []Player `bson:"users"`
-// }
