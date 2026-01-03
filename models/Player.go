f49c19d379d9d122ab0cde41bc41d14da53ce78f
unknown <darienmiller88@yahoo.com>
2026-01-03 11:06:44 -0500

    feat: added validation for player name to ensure it has exactly 2 parts

--- models/Player.go
+++ models/Player.go
@@ -2,6 +2,8 @@ package models
 
 import (
 	"database/sql"
+	"fmt"
+	"strings"
 	"time"
 
 	"github.com/go-ozzo/ozzo-validation/v4"
@@ -19,6 +21,22 @@ type Player struct{
 func (p *Player) Validate() error{
 	return validation.ValidateStruct(
 		p,
-		validation.Field(&p.PlayerName, validation.Length(3, 40)),
+		validation.Field(&p.PlayerName, validation.Required, validation.Length(5, 40), validation.By(p.validatePlayerNameHasTwoParts)),
 	)
+}
+
+func (p *Player) validatePlayerNameHasTwoParts(field interface{}) error{
+	playerName, ok := field.(string)
+
+	if !ok{
+		return fmt.Errorf("could not parse %T into object", field)
+	}
+	
+	fields := strings.Fields(playerName)
+
+	if len(fields) != 2 {
+		return fmt.Errorf("Player name must have exactly two parts: ex -> 'jane doe'")
+	}
+
+	return nil
 }
--- models/Player_test.go
+++ models/Player_test.go
@@ -1,1 +1,38 @@
 package models
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+//Test for getting all 8 locations
+//EXPECTED PAYLOAD:       All 8 locations
+//EXPECTED STATUS CODE:   200
+//EXPECTED ERROR MESSAGE: nil   
+func TestPlayerModelValidation_Ok(t *testing.T) {
+	player := Player{
+		PlayerName: "Test Name",
+	}
+
+	err := player.Validate()
+
+	assert.Equal(t, nil, err)
+}
+
+func TestPlayerModelValidation_NameTooShort(t *testing.T) {
+	
+}
+
+func TestPlayerModelValidation_NameTooLong(t *testing.T) {
+	
+}
+
+
+func TestPlayerModelValidation_NameHasOnePart(t *testing.T) {
+	
+}
+
+func TestPlayerModelValidation_NameHasThreeParts(t *testing.T) {
+	
+}
