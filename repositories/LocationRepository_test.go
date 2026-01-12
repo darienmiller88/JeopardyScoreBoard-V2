b788ea5821d459012771a8eca8c1cbb2ace1df6c
unknown <darienmiller88@yahoo.com>
2026-01-12 15:51:59 -0500

    feat: added TestGetAllLocations_Ok unit test

--- repositories/LocationRepository_test.go
+++ repositories/LocationRepository_test.go
@@ -1,3 +1,47 @@
 package repositories
 
-func 
+import (
+	"JeopardyScoreBoardV2/constants"
+	"net/http"
+	"regexp"
+	"testing"
+
+	"github.com/DATA-DOG/go-sqlmock"
+	"github.com/jmoiron/sqlx"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func setupLocationRepo(t *testing.T) (*sqlx.DB, sqlmock.Sqlmock, *sqlLocationRepository) {
+	db, mock, err := sqlmock.New()
+	require.NoError(t, err)
+
+	sqlxDB := sqlx.NewDb(db, "postgres")
+	repo := &sqlLocationRepository{db: sqlxDB}
+
+	t.Cleanup(func() {
+		db.Close()
+	})
+
+	return sqlxDB, mock, repo
+}
+
+func TestGetAllLocations_Ok(t *testing.T) {
+	_, mock, repo := setupLocationRepo(t)
+
+	rows := sqlmock.NewRows([]string{"location_name"}).
+		AddRow("Elmwood").
+		AddRow("Pelham Bay").
+		AddRow("Flushing")
+
+	mock.ExpectQuery(regexp.QuoteMeta(constants.GetAllLocations)).WillReturnRows(rows)
+
+	result := repo.GetAllLocations()
+
+	require.NoError(t, result.Err)
+	assert.Equal(t, http.StatusOK, result.StatusCode)
+	assert.Len(t, result.ResultData, 3)
+	assert.Equal(t, []string{"Elmwood", "Pelham Bay", "Flushing"}, result.ResultData)
+
+	require.NoError(t, mock.ExpectationsWereMet())
+}
