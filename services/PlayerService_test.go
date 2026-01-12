b248a68c8c9dff246d6c2fe465fe502b3ad1d282
unknown <darienmiller88@yahoo.com>
2026-01-12 09:43:24 -0500

    feat: added mock repo to Player service test

--- services/PlayerService_test.go
+++ services/PlayerService_test.go
@@ -1,1 +1,73 @@
 package services
+
+import (
+	"testing"
+
+	"JeopardyScoreBoardV2/models"
+)
+
+type mockPlayerRepository struct{
+	playerResult  models.Result[models.Player]
+	playersResult models.Result[[]models.Player]
+}
+
+func (m *mockPlayerRepository) UpdatePlayerName(oldPlayerName string, newPlayerName string) models.Result[models.Player]{
+	return m.playerResult
+}
+	
+func (m *mockPlayerRepository) AddPlayerToLocation(locationName string, playerName string)  models.Result[models.Player]{
+	return m.playerResult
+}
+
+func (m *mockPlayerRepository) RemovePlayer(playerName string) models.Result[models.Player] {
+	return m.playerResult
+}
+
+func (m *mockPlayerRepository) GetPlayersFromLocation(locationName string) models.Result[[]models.Player] {
+	return m.playersResult
+}
+
+func (m *mockPlayerRepository) GetAllPlayersFromAllLocations() models.Result[[]models.Player] {
+	return m.playersResult
+}
+
+
+////////////////////
+// CREATE/POST tests
+////////////////////
+
+func TestAddPlayer_Ok(t *testing.T){
+
+}
+
+func TestAddPlayer_PlayerNameTaken(t *testing.T){
+	
+}
+
+
+
+
+
+////////////////////
+// READ/GET tests
+////////////////////
+
+
+
+
+
+
+
+////////////////////
+// UPDATE/PUT tests
+////////////////////
+
+
+
+
+
+
+
+////////////////////
+// DESTROY/DELETE tests
+////////////////////
