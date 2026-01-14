58d790b35e714cb3c3416feb383ac04ee3549eb4
unknown <darienmiller88@yahoo.com>
2026-01-14 16:23:13 -0500

    chore: added new goals to checklist

--- controllers/Index.go
+++ controllers/Index.go
@@ -21,7 +21,7 @@ func (i *Index) InitControllers(db *sqlx.DB){
 	//Initialize the views controller
 	i.viewsController.Init()
 
-	//Initialize the jeopardy controller, and chose the service and repo implementation
+	//Initialize the controllers, and choose the service and repo implementation
 	i.locationsController.Init(&services.LocationServiceImpl{ Repository: repositories.GetSqlLocationRepository(db) })
 	i.playersController.Init(&services.PlayerServiceImpl{ Repository: repositories.GetSqlPlayerRepository(db) })
 
