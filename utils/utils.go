aa14b8ee495b06033d0c34334229a100c49dd205
unknown <darienmiller88@yahoo.com>
2026-01-23 22:53:37 -0500

    progress: added util.go to staging

---
+++ utils/utils.go
@@ -0,0 +1,12 @@
+package utils
+
+import "JeopardyScoreBoardV2/models"
+
+//Helper function to allow repos to send result payloads with less text.
+func GetResult[T any](err error, statusCode int, payload T) models.Result[T] {
+	return models.Result[T]{
+		StatusCode: statusCode,
+		Err: err,
+		ResultData: payload,
+	}
+}
