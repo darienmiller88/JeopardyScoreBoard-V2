5920acaf10c5fc2b7c7a91db37ab84014a377664
unknown <darienmiller88@yahoo.com>
2026-02-10 16:01:10 -0500

    fix: test seeding finally fixed

--- test-migrations/000002_location_and_team_seeding.up.sql
+++ test-migrations/000002_location_and_team_seeding.up.sql
@@ -18,44 +18,38 @@ ON CONFLICT (location_name) DO NOTHING;
 -- Seed Players
 -- ============================
 INSERT INTO players (
-    location_id
+    location_id,
     player_name_encrypted,
     player_name_hash
 ) 
 VALUES
 (
-    (SELECT id FROM locations WHERE location_name='Pelham Bay')
+    (SELECT id FROM locations WHERE location_name='Pelham Bay'),
     decode('Ex5oYOXNL5Y8m0f7YIqRUA+ZWgjDRZhPV8tOPCGXFtPgE1u75Thp9i0=', 'base64'),-- goofer boofer
-    decode('xbdAEiZPI8Bk4uKdBwqswauh25BBd3F5RXJ0AGMivqE=', 'base64'),
+    decode('xbdAEiZPI8Bk4uKdBwqswauh25BBd3F5RXJ0AGMivqE=', 'base64')
 ),
 (
-    (SELECT id FROM locations WHERE location_name='Elmwood')
+    (SELECT id FROM locations WHERE location_name='Elmwood'),
     decode('VrjKjrSWIAAnsA0k1m/e9ItWb8Txm8VtX9MeEULtAAemufa62t8=', 'base64'),-- new player
-    decode('UAA5Do+JyUCUuFexLz2aQgiJPcDudRCURW+U0k9S9nE=', 'base64'),
+    decode('UAA5Do+JyUCUuFexLz2aQgiJPcDudRCURW+U0k9S9nE=', 'base64')
 ),
 (
-    (SELECT id FROM locations WHERE location_name='Lawrence')
+    (SELECT id FROM locations WHERE location_name='Lawrence'),
     decode('Tl5Y+RZYaH16CePouN3Y8sy9s/+QBCvr4x2MJGtIvBk8TtEauLc=', 'base64'),--player one
-    decode('s9XALDA7x7nATlrx/hMCYW2gjfoBdFXeitf+EOgULfI=', 'base64'),
+    decode('s9XALDA7x7nATlrx/hMCYW2gjfoBdFXeitf+EOgULfI=', 'base64')
 ),
 (
-    (SELECT id FROM locations WHERE location_name='Lawrence')
+    (SELECT id FROM locations WHERE location_name='Lawrence'),
     decode('o/y9bLs16Ta+s/JE2/6f3M1+cuCXFfgoSrEBF8wvxR0lcaFQ1xA=', 'base64'),--player mah
-    decode('s/+tK2VVjdvan2ryG33pA8cEpYwrscbJMdF1SY8dWXI=', 'base64'),  
+    decode('s/+tK2VVjdvan2ryG33pA8cEpYwrscbJMdF1SY8dWXI=', 'base64')
 );
 
 -- ============================
 -- Create one Team per Location
 -- ============================
-INSERT INTO teams (location_id) VALUES((SELECT id FROM locations WHERE location_name='Elmwood'));
-INSERT INTO teams (location_id) VALUES((SELECT id FROM locations WHERE location_name='Lawrence'));
-INSERT INTO teams (location_id) VALUES((SELECT id FROM locations WHERE location_name='Flushing'));
-INSERT INTO teams (location_id) VALUES((SELECT id FROM locations WHERE location_name='Grand Concourse'));
-INSERT INTO teams (location_id) VALUES((SELECT id FROM locations WHERE location_name='Port Richmond'));
-INSERT INTO teams (location_id) VALUES((SELECT id FROM locations WHERE location_name='5030 Broadway'));
-INSERT INTO teams (location_id) VALUES((SELECT id FROM locations WHERE location_name='W 154th St'));
-INSERT INTO teams (location_id) VALUES((SELECT id FROM locations WHERE location_name='Pelham Bay'));
-
+INSERT INTO teams (location_id)
+SELECT id FROM locations
+ON CONFLICT (location_id) DO NOTHING;
 
 -- =============================
 -- Seed player saved games
@@ -64,6 +58,7 @@ INSERT INTO savedgames (
     location_id,
     winning_player_id,
     winning_player_name_encrypted,
+    winning_player_name_hash,
     total_score,
     average_score
 )
@@ -72,6 +67,7 @@ VALUES
     (SELECT id FROM locations WHERE location_name = 'Elmwood'),
     (SELECT id FROM players WHERE player_name_hash = decode('xbdAEiZPI8Bk4uKdBwqswauh25BBd3F5RXJ0AGMivqE=', 'base64')),
     decode('Ex5oYOXNL5Y8m0f7YIqRUA+ZWgjDRZhPV8tOPCGXFtPgE1u75Thp9i0=', 'base64'),-- goofer boofer
+    decode('xbdAEiZPI8Bk4uKdBwqswauh25BBd3F5RXJ0AGMivqE=', 'base64'),
     1200,
     400.0
 ),
@@ -79,6 +75,7 @@ VALUES
     (SELECT id FROM locations WHERE location_name = 'Lawrence'),
     (SELECT id FROM players WHERE player_name_hash = decode('UAA5Do+JyUCUuFexLz2aQgiJPcDudRCURW+U0k9S9nE=', 'base64')),
     decode('VrjKjrSWIAAnsA0k1m/e9ItWb8Txm8VtX9MeEULtAAemufa62t8=', 'base64'), -- new player
+    decode('UAA5Do+JyUCUuFexLz2aQgiJPcDudRCURW+U0k9S9nE=', 'base64'),
     2200,
     600.25
 ),
@@ -86,6 +83,7 @@ VALUES
     (SELECT id FROM locations WHERE location_name = 'Flushing'),
     (SELECT id FROM players WHERE player_name_hash = decode('s9XALDA7x7nATlrx/hMCYW2gjfoBdFXeitf+EOgULfI=', 'base64')),
     decode('Tl5Y+RZYaH16CePouN3Y8sy9s/+QBCvr4x2MJGtIvBk8TtEauLc=', 'base64'), --player one
+    decode('s9XALDA7x7nATlrx/hMCYW2gjfoBdFXeitf+EOgULfI=', 'base64'),
     2200,
     600.25
 ),
@@ -93,6 +91,7 @@ VALUES
     (SELECT id FROM locations WHERE location_name = 'Lawrence'),
     (SELECT id FROM players WHERE player_name_hash = decode('s/+tK2VVjdvan2ryG33pA8cEpYwrscbJMdF1SY8dWXI=', 'base64')),
     decode('o/y9bLs16Ta+s/JE2/6f3M1+cuCXFfgoSrEBF8wvxR0lcaFQ1xA=', 'base64'),--player mah  
+    decode('s/+tK2VVjdvan2ryG33pA8cEpYwrscbJMdF1SY8dWXI=', 'base64'),
     2200,
     600.25
 );
