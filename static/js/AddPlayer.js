e26021dfa176431857dbf95d5922c3d0d10ddc8d
darienmiller88 <darienmiller88@yahoo.com>
2026-02-27 19:23:30 -0500

    feat: added player card info to delete and edit modals

--- static/js/AddPlayer.js
+++ static/js/AddPlayer.js
@@ -12,6 +12,7 @@ function enableForm(form, duration) {
     }, duration);
 }
 
+// Disables the form to prevent multiple submissions. Of course, routes will be rate limited as well.
 function disableForm(form, duration) {
     const inputs = form.querySelectorAll('input, select, button');
     inputs.forEach(input => {
@@ -34,6 +35,7 @@ function disableForm(form, duration) {
     }, duration);
 }
 
+// Function to show toast notification for the client.
 function showToast(message, type = 'success', duration = 2000) {
     const toast = document.getElementById('toast');
     
@@ -63,4 +65,39 @@ function showToast(message, type = 'success', duration = 2000) {
 function hideToast() {
     const toast = document.getElementById('toast');
     toast.classList.remove('show');
+}
+
+function populateEditModal(button) {
+    const playerId = button.dataset.playerId;
+    const names = button.dataset.playerName.split(" ");
+    const firstName = names[0]
+    const lastName = names[1]
+    
+    //populate the edit player name form with the name of the player
+    document.getElementById('edit-player-id').value = playerId;
+    document.getElementById('edit-first-name').value = firstName;
+    document.getElementById('edit-last-name').value = lastName;
+    
+    // Update the HTMX button with correct player ID
+    const saveBtn = document.getElementById('confirm-edit-btn');
+    saveBtn.setAttribute('hx-put', `/players/${playerId}`);
+    saveBtn.setAttribute('hx-target', `#${playerId}`)
+}
+
+function populateDeleteModal(button) {
+    const playerId = button.dataset.playerId;
+    const playerName = button.dataset.playerName;
+    
+    //Add the player name to the delete player modal
+    document.getElementById('delete-player-name').textContent = playerName;
+    
+    const deleteBtn = document.getElementById('confirm-delete-btn');
+    
+    // Update the delete button with correct URL and target htmx attributes
+    deleteBtn.setAttribute('hx-delete', `/players/${playerId}`);
+    deleteBtn.setAttribute('hx-target', `#${playerId}`)
+}
+
+function savePlayerEdit() {
+    // Optional: Add validation or additional logic here
 }
