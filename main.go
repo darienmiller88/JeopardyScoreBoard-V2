8ceb10775e014bcaae7d0b5e86b624394898ac2d
DarienMiller <darienmiller88@yahoo.com>
2026-03-17 23:04:30 -0400

    push from old hp agai

--- main.go
+++ main.go
@@ -20,7 +20,7 @@ import (
 )
 
 //push from work laptop. changed my author email
-//push from old hp
+//push from old hp, oligatory push of thr day
 func main() {
 	//Load env file immediately at the start of the program
 	godotenv.Load()
