ae4c7911b25e22d5d6c58b09f73924cac7a9f86f
Darien Miller <darienmiller88@yahoo.com>
2026-05-22 15:29:03 -0400

    padding out

--- main.go
+++ main.go
@@ -23,7 +23,7 @@ import (
 
 //push from work laptop. changed my author email
 //push from old hp, oligatory push of thr day pt 3 lol
-//gonna try to understand what i did
+//gonna try to understand what i did. and wrap up this project
 func main() {
 	//Load env file immediately at the start of the program
 	godotenv.Load()
